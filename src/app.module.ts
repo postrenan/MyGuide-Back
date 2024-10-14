9fb9a797ba92229ca31fb7eb89248238bcb4f848
Bick Renan <renan.bick@acad.ufsm.br>
2024-10-14 20:47:14 -0300

    feat: adição dos modulos estabelecidos para o projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { LocationModule } from './location/location.module';
+import { ShopModule } from './shop/shop.module';
+import { ProductModule } from './product/product.module';
+import { FavoriteModule } from './favorite/favorite.module';
+import { ReviewModule } from './review/review.module';
+import { CategoryModule } from './category/category.module';
+import { PictureModule } from './picture/picture.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AdminModule, LocationModule, ShopModule, ProductModule, FavoriteModule, ReviewModule, CategoryModule, PictureModule],
   controllers: [AppController],
   providers: [AppService],
 })
