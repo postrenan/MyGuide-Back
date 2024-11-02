d2b03ec23901aaa36352d376567439e676c32308
Bick Renan <renan.bick@acad.ufsm.br>
2024-11-02 11:50:07 -0300

    fix: cors edit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
