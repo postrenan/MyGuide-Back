a8a2766bdc13f3c299053796ef191a3998477729
Bick Renan <renan.bick@acad.ufsm.br>
2024-12-09 20:17:49 -0300

    fix: delete de banco

--- src/shop/dto/create-shop.dto.ts
+++ src/shop/dto/create-shop.dto.ts
@@ -1,1 +1,10 @@
-export class CreateShopDto {}
+
+export class CreateShopDto {
+  
+    name: string;
+    description: string;
+    price?: string;  
+    openTime: Date;
+    closeTime: Date;
+
+  }
